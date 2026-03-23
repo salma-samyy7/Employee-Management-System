b6c19e6ef9daffbca931823ec94be070d9cbfbc8
salma-samyy7 <salma.samyy26@gmail.com>
2026-03-23 17:31:23 +0200

    EmployeeManager 1st update

--- src/EmployeeManager.java
+++ src/EmployeeManager.java
@@ -1,2 +1,22 @@
+import java.util.ArrayList;
+
 public class EmployeeManager {
+    ArrayList<Employee> employees ;
+
+    public EmployeeManager(ArrayList<Employee> employees) {
+        this.employees = employees;
+    }
+    public ArrayList<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void addEmployee(Employee employee)
+    {
+        employees.add(employee);
+    }
+    public void removeEmployee(Employee employee)
+    {
+        employees.remove(employee);
+    }
+
 }
